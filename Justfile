eb00113e302ee4f8c1edf5c6c10a92875d0317a4
ChromaMaster <vladtel.dev@gmail.com>
2024-05-12 20:18:02 +0200

    test(http): add test for status code 404
    Signed-off-by: Vlad Alexandru Teletin <vladtel.dev@gmail.com>

--- Justfile
+++ Justfile
@@ -4,10 +4,10 @@ run:
 	zig build run
 
 test-setup:
-	podman run -d --name zig-test-setup -p 35556:80 docker.io/kennethreitz/httpbin
+	podman run -d --name zig-test-setup -p 35565:80 docker.io/kennethreitz/httpbin
 
 test: test-setup && test-teardown
-	find ./src -type f -iname "*.zig" | xargs -i zig test {}
+	-find ./src -type f -iname "*.zig" | xargs -i zig test {}
 
 test-teardown:
 	podman stop zig-test-setup && podman rm zig-test-setup
--- src/http_test.zig
+++ src/http_test.zig
@@ -1,15 +1,29 @@
 const http = @import("http.zig");
 
-const testing = @import("std").testing;
+const std = @import("std");
+const testing = std.testing;
 const allocator = testing.allocator;
 
-test "HTTP GET returns correct status code" {
+const host = "http://localhost:35565";
+
+test "HTTP GET returns correct status code 200" {
     const client = http.Client.init(allocator);
 
-    const response = try client.get("http://localhost:35556/get");
+    const response = try client.get(host ++ "/get");
     defer response.?.deinit();
 
     try testing.expect(response.?.status == 200);
+
+    try testing.expect(1 == 1);
+}
+
+test "HTTP GET returns correct status code 404" {
+    const client = http.Client.init(allocator);
+
+    const response = try client.get(host ++ "/not-found");
+    defer response.?.deinit();
+
+    try testing.expect(response.?.status == 404);
 
     try testing.expect(1 == 1);
 }
